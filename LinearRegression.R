533f3d94ec87e0e03a64bcba5d200f21166adcbd
Josh Kraan <joshua.kraan.jk@gmail.com>
2017-04-19 20:20:14 -0700

    Improvements to speed brought by switch to using dplyr and lsfit, possibly find faster solution in future.

--- LinearRegression.R
+++ LinearRegression.R
@@ -4,6 +4,9 @@ library("scales")
 library("reshape2")
 library("nlme")
 
+#possibly remove
+library("dplyr")
+
 # Give data as csv file with columns: x, xuncertainty, y, yuncertainty
 # If you have a header on your file set header = TRUE below.
 
@@ -25,8 +28,8 @@ normaldistribution = function(n, mean, uncertainty) {
 }
 
 #Change the n value for these for more accuracy. Values of 10000 or higher seem to work well, but take some time.
-xdata = normaldistribution(1000, data[,1], data[,2])
-ydata = normaldistribution(1000, data[,3], data[,4])
+xdata = normaldistribution(100000, data[,1], data[,2])
+ydata = normaldistribution(100000, data[,3], data[,4])
 
 yerrors = aes(ymax = data[,3] + data[,4], ymin = data[,3] - data[,4])
 xerrors = aes(xmax = data[,1] + data[,2], xmin = data[,1] - data[,2])
@@ -35,20 +38,25 @@ mergeddata = cbind(xdata, ydata)
 mergeddata[,3] = NULL
 names(mergeddata) = c("Index", "xValue", "yValue")
 
-regressions = coef(lmList(yValue ~ xValue | Index, data = mergeddata, pool = FALSE))
 
-bestlineslope = mean(regressions[,2])
-bestlineintercept = mean(regressions[,1])
+regressions = mergeddata %>% group_by(Index) %>% do(data.frame(as.list(coef(lsfit(matrix(.$xValue), .$yValue)))))
+#regressions = coef(lmList(yValue ~ xValue | Index, data = mergeddata, pool = FALSE))
+
+slopevalues = as.numeric(unlist(regressions[,3]))
+interceptvalues = as.numeric(unlist(regressions[,2]))
+
+bestlineslope = mean(slopevalues)
+bestlineintercept = mean(interceptvalues)
 
 #Change to 2 or 3 sd depending on accuracy desired.
-slopeUncertainty = 3*sd(regressions[,2])
-interceptUncertainty = 3*sd(regressions[,1])
+slopeUncertainty = 3*sd(slopevalues)
+interceptUncertainty = 3*sd(interceptvalues)
 
-highslope = mean(regressions[,2]) + slopeUncertainty
-highintercept = mean(regressions[,1]) + interceptUncertainty
+highslope = bestlineslope + slopeUncertainty
+highintercept = bestlineintercept + interceptUncertainty
 
-lowslope = mean(regressions[,2]) - slopeUncertainty
-lowintercept = mean(regressions[,1]) - interceptUncertainty
+lowslope = bestlineslope - slopeUncertainty
+lowintercept = bestlineintercept - interceptUncertainty
 
 plot1 = 
   qplot(data[,1], data[,3]) + 
@@ -58,15 +66,15 @@ plot1 =
   geom_errorbarh(xerrors, height = 0.3) + 
   geom_abline(intercept = bestlineintercept, slope = bestlineslope) +
   geom_abline(intercept = highintercept, slope = lowslope) + 
-  geom_abline(intercept = lowintercept, slope = highslope) + 
+  geom_abline(intercept = lowintercept, slope = highslope) #+ 
   #Below shows the generated data in red.
-  geom_point(aes(xdata[,2], ydata[,2]), color = "red", alpha = 1/150) +
+  #geom_point(aes(xdata[,2], ydata[,2]), color = "red", alpha = 1/150) +
   #Below shows the generated regressions lines in grey.
-  geom_abline(intercept = regressions[,1], slope = regressions[,2], alpha = 1/50, color = "grey")
+  #geom_abline(intercept = interceptvalues, slope = slopevalues, alpha = 1/50, color = "grey")
 
 print(plot1)
 
-cat("Slope: ", bestlineslope, "Slope Uncertainty: ", slopeUncertainty, 
+cat("\nSlope: ", bestlineslope, "Slope Uncertainty: ", slopeUncertainty, 
     "\nIntercept: ", bestlineintercept, "Intercept Uncertainty: ", interceptUncertainty)
 
 #Some interesting density plots:
