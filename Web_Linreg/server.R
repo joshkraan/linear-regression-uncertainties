358bd02b5caa8fc1ffa05a372e995fb0acb8aa80
Josh Kraan <joshua.kraan.jk@gmail.com>
2017-04-21 21:58:15 -0700

    Switched ui to using shinydashboard.

--- Web_Linreg/server.R
+++ Web_Linreg/server.R
@@ -4,6 +4,9 @@ library(ggthemes)
 library(scales)
 library(reshape2)
 library(nlme)
+library(dplyr)
+library(Cairo)
+options(shiny.usecairo=T)
 
 #TODO: Seperate graph and regression calculations
 #TODO: Use ggsave()
@@ -30,117 +33,126 @@ shinyServer(function(input, output) {
       
   observeEvent(input$calculateFit, {
     
-    withProgress(message = "Calculating Fit", value = 1, {
+    validate(
+      need(input$setNumber >= 100 && input$setNumber <= 100000, "Please select a number from 100 to 100,000")
+    )
+    
+    samples = input$setNumber
+    
+    inputFile = input$csvFile
+    
+    if (is.null(inputFile))
+      return(NULL)
+    
+    #Read uploaded data file. Uses header if option is selected in UI.
+    data = read.csv(inputFile$datapath, header = input$header)
+    
+    normaldistribution = function(n, mean, uncertainty) {
+      #Standard Deviation can be approximated to range/4 with 95% accuracy, range = 2*uncertainty, so sd equals approximately unc/2
+      # Two or 3 standard deviations?
+      sd = uncertainty/3
+      #rnorm() can take vectors (term for lists), will cycle through them
+      result = rnorm(n*length(mean), mean = mean, sd = sd)
+      #Create matrix of results. Each row is a point, and each column is a generated set of points.
+      resultmatrix = matrix(result, , ncol = n, byrow = FALSE)
+      #Reorganize the result
+      melteddata = melt(resultmatrix)
+      melteddata[,1] = NULL
+      return(melteddata)
+    }
+    
+    xdata = normaldistribution(samples, data[,1], data[,2])
+    ydata = normaldistribution(samples, data[,3], data[,4])
+    
+    yerrors = aes(ymax = data[,3] + data[,4], ymin = data[,3] - data[,4])
+    xerrors = aes(xmax = data[,1] + data[,2], xmin = data[,1] - data[,2])
+    
+    mergeddata = cbind(xdata, ydata)
+    mergeddata[,3] = NULL
+    names(mergeddata) = c("Index", "xValue", "yValue")
+    
+    withProgress(message = "Computing Regressions...", {
+      regressionfunction = function(x){
+        incProgress(1/samples)
+        result = lsfit(matrix(x$xValue), x$yValue)
+        return(data.frame(as.list(coef(result))))
+      }
+      
+      regressions = mergeddata %>% group_by(Index) %>% do(regressionfunction(.))
+    })
+  
+    #regressions = coef(lmList(yValue ~ xValue | Index, data = mergeddata, pool = FALSE))
+    
+    slopevalues = as.numeric(unlist(regressions[,3]))
+    interceptvalues = as.numeric(unlist(regressions[,2]))
+    
+    bestlineslope = mean(slopevalues)
+    bestlineintercept = mean(interceptvalues)
+    
+    slopeUncertainty = 3*sd(slopevalues)
+    interceptUncertainty = 3*sd(interceptvalues)
+    
+    highslope = bestlineslope + slopeUncertainty
+    highintercept = bestlineintercept + interceptUncertainty
+    
+    lowslope = bestlineslope - slopeUncertainty
+    lowintercept = bestlineintercept - interceptUncertainty
+    
+    output$scatterPlot = renderPlot({
       
       inputFile = input$csvFile
       
-      if (is.null(inputFile))
+      if (is.null(inputFile)){
         return(NULL)
-      
-      #Read uploaded data file. Uses header if option is selected in UI.
-      data = read.csv(inputFile$datapath, header = input$header)
-      
-      normaldistribution = function(n, mean, uncertainty) {
-        #Standard Deviation can be approximated to range/4 with 95% accuracy, range = 2*uncertainty, so sd equals approximately unc/2
-        # Two or 3 standard deviations?
-        sd = uncertainty/3
-        #rnorm() can take vectors (term for lists), will cycle through them
-        result = rnorm(n*length(mean), mean = mean, sd = sd)
-        #Create matrix of results. Each row is a point, and each column is a generated set of points.
-        resultmatrix = matrix(result, , ncol = n, byrow = FALSE)
-        #Reorganize the result
-        melteddata = melt(resultmatrix)
-        melteddata[,1] = NULL
-        return(melteddata)
       }
       
-      xdata = normaldistribution(input$setNumber, data[,1], data[,2])
-      ydata = normaldistribution(input$setNumber, data[,3], data[,4]) # How many of these for good accuracy?
+      data = read.csv(inputFile$datapath, header = input$header)
       
-      yerrors = aes(ymax = data[,3] + data[,4], ymin = data[,3] - data[,4])
-      xerrors = aes(xmax = data[,1] + data[,2], xmin = data[,1] - data[,2])
+      plot1 = 
+        qplot(data[,1], data[,3]) + 
+        geom_errorbar(yerrors, width = 0.2)  + 
+        geom_errorbarh(xerrors, height = 0.3) + 
+        geom_abline(intercept = bestlineintercept, slope = bestlineslope)
       
-      mergeddata = cbind(xdata, ydata)
-      mergeddata[,3] = NULL
-      names(mergeddata) = c("Index", "xValue", "yValue")
+      xrange = layer_scales(plot1)$x$range$range[2] - layer_scales(plot1)$x$range$range[1]
+      yrange = layer_scales(plot1)$y$range$range[2] - layer_scales(plot1)$y$range$range[1]
       
-      #This takes the most time.
-      regressions = coef(lmList(yValue ~ xValue | Index, data = mergeddata, pool = FALSE))
-      #See https://stat.ethz.ch/R-manual/R-devel/library/nlme/html/intervals.lmList.html for info on confidence intervals in Lmlist
-      #Used to get confidence interval for each individual line fit 
-      #Possibly weight by uncertainty?
-      #See: https://stackoverflow.com/questions/14636052/using-smooth-in-ggplot2-to-fit-a-linear-model-using-the-errors-given-in-the-data
+      equationLabel = paste("Slope:\n", bestlineslope, "\u00B1", slopeUncertainty, "\nIntercept:\n", bestlineintercept,
+                            "\u00B1", interceptUncertainty)
       
-      
-      bestlineslope = mean(regressions[,2])
-      bestlineintercept = mean(regressions[,1])
-      
-      slopeUncertainty = 3*sd(regressions[,2])
-      interceptUncertainty = 3*sd(regressions[,1])
-      
-      highslope = mean(regressions[,2]) + slopeUncertainty
-      highintercept = mean(regressions[,1]) + interceptUncertainty
-      
-      lowslope = mean(regressions[,2]) - slopeUncertainty
-      lowintercept = mean(regressions[,1]) - interceptUncertainty
-      
-      output$scatterPlot = renderPlot({
-        
-        inputFile = input$csvFile
-        
-        if (is.null(inputFile)){
-          return(NULL)
-        }
-        
-        data = read.csv(inputFile$datapath, header = input$header)
-        
-        plot1 = 
-          qplot(data[,1], data[,3]) + 
-          geom_errorbar(yerrors, width = 0.2)  + 
-          geom_errorbarh(xerrors, height = 0.3) + 
-          geom_abline(intercept = bestlineintercept, slope = bestlineslope)
-        
-        xrange = layer_scales(plot1)$x$range$range[2] - layer_scales(plot1)$x$range$range[1]
-        yrange = layer_scales(plot1)$y$range$range[2] - layer_scales(plot1)$y$range$range[1]
-        
-        equationLabel = paste("Slope:\n", bestlineslope, "\u00B1", slopeUncertainty, "\nIntercept:\n", bestlineintercept,
-                              "\u00B1", interceptUncertainty)
-        
-        if(input$showSpread == TRUE){
-          plot1 = 
-            plot1 + 
-            geom_abline(intercept = regressions[,1], slope = regressions[,2], alpha = 1/10, color = input$spreadColor)
-        }
-        
-        if(input$showGenerated == TRUE){
-          plot1 = 
-            plot1 +
-            geom_point(aes(xdata[,2], ydata[,2]), color = input$dataColor, alpha = 1/20)
-        }
-        
-        if(input$showMaxMin == TRUE){
-          plot1 = 
-            plot1 +
-            geom_abline(intercept = highintercept, slope = lowslope) + 
-            geom_abline(intercept = lowintercept, slope = highslope)
-        }
-        
-        if(input$showEquationFloat == TRUE){
-          xoffset = (input$equationHorizontal/100)*xrange
-          yoffset = (input$equationVertical/100)*yrange
-          
-          plot1 = 
-            plot1 +
-            geom_label(aes(xoffset, yoffset, label = equationLabel, hjust = "left"), label.r = unit(0, "lines"))
-        }
-        
+      if(input$showSpread == TRUE){
         plot1 = 
           plot1 + 
-          theme_bw()
+          geom_abline(intercept = interceptvalues, slope = slopevalues, alpha = 1/10, color = input$spreadColor)
+      }
+      
+      if(input$showGenerated == TRUE){
+        plot1 = 
+          plot1 +
+          geom_point(aes(xdata[,2], ydata[,2]), color = input$dataColor, alpha = 1/20)
+      }
+      
+      if(input$showMaxMin == TRUE){
+        plot1 = 
+          plot1 +
+          geom_abline(intercept = highintercept, slope = lowslope) + 
+          geom_abline(intercept = lowintercept, slope = highslope)
+      }
+      
+      if(input$showEquationFloat == TRUE){
+        xoffset = (input$equationHorizontal/100)*xrange
+        yoffset = (input$equationVertical/100)*yrange
         
-        print(plot1)
-        
-      })
+        plot1 = 
+          plot1 +
+          annotate("text", xoffset, yoffset, label = equationLabel)
+      }
+      
+      plot1 = 
+        plot1 + 
+        theme_bw()
+      
+      plot1
       
     })
 
--- Web_Linreg/ui.R
+++ Web_Linreg/ui.R
@@ -1,46 +1,30 @@
-
-
 library(shiny)
 library(colourpicker)
+library(shinydashboard)
 
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Linear Regression With Uncertainties"),
-  
-  fluidRow(
-    # Sidebar with file input options.
-    column(3,
-        wellPanel(
-          #This is the file options panel.
-          h4("File Options", align = "center"),
-          tags$hr(),
-          fileInput("csvFile", "Select a CSV file to graph", 
-                    accept = c(
-                        'text/csv',
-                        'text/comma-separated-values',
-                        '.csv'
-                    )
-          ),
-          # Add a horizontal line
-          tags$hr(),
-          checkboxInput('header', 'Header', FALSE)
-        ),
-        
-        wellPanel(
-          #This is the regression options panel
-          h4("Regression Options", align = "center"),
-          tags$hr(),
-          #TODO: Figure out a max for Number Input
-          numericInput("setNumber", "Generated Sets", min = 100, value = 100),
-          tags$hr(),
-          actionButton("calculateFit", "Calculate Fit", width = '100%')
-        ),
-        
-        wellPanel(
-          #This is the graph options panel. 
-          h4("Graph Options", align = "center"),
-          tags$hr(),
+sidebar = dashboardSidebar(
+  sidebarMenu(id = "menu",
+    menuItem("Graph", tabName = "graph", icon = icon("line-chart")),
+    menuItem("Table", tabName = "table", icon = icon("table"))
+  ),
+  div(style = "height: 75px", fileInput("csvFile", "Select a CSV file to graph",
+            accept = c(
+              'text/csv',
+              'text/comma-separated-values',
+              '.csv'
+            )
+  )),
+  checkboxInput('header', 'Header', FALSE),
+  numericInput("setNumber", "Number of Generated Sets", min = 100, max = 100000, value = 100),
+  div(style = "text-align: center", actionButton("calculateFit", "Calculate Fit", width = '85%'))
+)
+
+body = dashboardBody(
+  tabItems(
+    tabItem(tabName = "graph", {
+      fluidRow(
+        box(width = "9", plotOutput("scatterPlot")),
+        box(width = "3",
           checkboxInput("showMaxMin", "Show Max/Min Lines", value = FALSE),
           checkboxInput("showSpread", "Show Spread", value = FALSE),
           conditionalPanel(
@@ -62,20 +46,91 @@ shinyUI(fluidPage(
             sliderInput("equationHorizontal", "Horizontal Distance", min = 0, max = 100, value = 0, post = "%")
           )
         )
-    ),
-    
-    # Show a plot of the generated distribution
-    column(9,
-      tabsetPanel(tabPanel("Plot", 
-                           {
-                             if(is.null(plotOutput("scatterPlot"))){
-                               h4("test")
-                             } else {
-                               plotOutput("scatterPlot")
-                               #h4("test", align = "center")
-                             }
-                           }),
-                  tabPanel("Table", tableOutput("dataTable")))
-    )
+      )
+    }),
+    tabItem(tabName = "table", tableOutput("dataTable"))
   )
-))
+)
+
+dashboardPage(
+  dashboardHeader(title = "Linear Regression"),
+  sidebar,
+  body
+)
+
+# shinyUI(fluidPage(
+#   
+#   # Application title
+#   titlePanel("Linear Regression With Uncertainties"),
+#   
+#   fluidRow(
+#     # Sidebar with file input options.
+#     column(3,
+#         wellPanel(
+#           #This is the file options panel.
+#           h4("File Options", align = "center"),
+#           tags$hr(),
+          # fileInput("csvFile", "Select a CSV file to graph",
+          #           accept = c(
+          #               'text/csv',
+          #               'text/comma-separated-values',
+          #               '.csv'
+          #           )
+          # ),
+          # # Add a horizontal line
+          # tags$hr(),
+          # checkboxInput('header', 'Header', FALSE)
+#         ),
+#         
+#         wellPanel(
+#           #This is the regression options panel
+#           h4("Regression Options", align = "center"),
+          # tags$hr(),
+          # #TODO: Figure out a max for Number Input
+          # numericInput("setNumber", "Generated Sets", min = 100, value = 100),
+          # tags$hr(),
+          # actionButton("calculateFit", "Calculate Fit", width = '100%')
+#         ),
+#         
+#         wellPanel(
+#           #This is the graph options panel. 
+#           h4("Graph Options", align = "center"),
+#           tags$hr(),
+          # checkboxInput("showMaxMin", "Show Max/Min Lines", value = FALSE),
+          # checkboxInput("showSpread", "Show Spread", value = FALSE),
+          # conditionalPanel(
+          #   condition = "input.showSpread == true",
+          #   colourInput("spreadColor", "Spread Color", value = "grey")
+          # ),
+          # checkboxInput("showGenerated", "Show Generated Data", value = FALSE),
+          # conditionalPanel(
+          #   condition = "input.showGenerated == true",
+          #   colourInput("dataColor", "Generated Data Color", value = "red")
+          # ),
+          # checkboxInput("showEquationFloat", "Show Equation On Graph", value = FALSE),
+          # conditionalPanel(
+          #   condition = "input.showEquationFloat == true",
+          #   sliderInput("equationVertical", "Vertical Distance", min = 0, max = 100, value = 0, post = "%")
+          # ),
+          # conditionalPanel(
+          #   condition = "input.showEquationFloat == true",
+          #   sliderInput("equationHorizontal", "Horizontal Distance", min = 0, max = 100, value = 0, post = "%")
+          # )
+#         )
+#     ),
+#     
+#     # Show a plot of the generated distribution
+#     column(9,
+#       tabsetPanel(tabPanel("Plot", 
+#                            {
+#                              if(is.null(plotOutput("scatterPlot"))){
+#                                h4("test")
+#                              } else {
+#                                plotOutput("scatterPlot")
+#                                #h4("test", align = "center")
+#                              }
+#                            }),
+#                   tabPanel("Table", tableOutput("dataTable")))
+#     )
+#   )
+# ))
