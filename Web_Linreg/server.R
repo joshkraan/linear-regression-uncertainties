d6c55289de6ee56443198b639ab13633d570a5dd
Josh Kraan <joshua.kraan.jk@gmail.com>
2017-08-13 20:12:01 -0700

    Made graphs look the same accross all devices. Changed from ggsave() to using classic pdf(), png(), and svg() functions, fixing issues with PPI not being the same in downloads. Added units to graph labels and to the equation float. Cleaned up the code by moving plot definition to a seperate reactive() function.

--- Web_Linreg/server.R
+++ Web_Linreg/server.R
@@ -12,24 +12,27 @@ library(DT)
 
 shinyServer(function(input, output, session) {
   
-  #Disable the download button until sufficient options are set.
-  shinyjs::disable("downloadPlot")
+  # Disable the download button until the file is uploaded.
   
-  #Read the uploaded data file and produce a visual data table.
+  shinyjs::disable("downloadPlot")
+  observe({
+    if(!is.null(input$csvFile)) {
+      shinyjs::enable("downloadPlot")
+    }
+  })
+  
+  # Read the uploaded data file and produce a visual data table.
+  
   output$dataTable = DT::renderDataTable(options = list(columnDefs = list(list(className = 'dt-left', targets = "_all"))), {
-    inputFile = input$csvFile
     
-    if (is.null(inputFile)) {
+    if (is.null(input$csvFile)) {
       return(data.frame(X = numeric(0), Y = numeric(0)))
     }
     
-    
     #TODO: Use readr
-    data = read.csv(inputFile$datapath, header = input$header)
+    data = read.csv(input$csvFile$datapath, header = input$header)
     
     names(data) = c('XValue', 'XUncertainty', 'YValue', 'YUncertainty')
-    
-    #TODO: clean up the following
     
     data %<>% unite(X, XValue, XUncertainty, sep = "\u00B1", remove = TRUE)
     data %<>% unite(Y, YValue, YUncertainty, sep = "\u00B1", remove = TRUE)
@@ -37,7 +40,8 @@ shinyServer(function(input, output, session) {
     data
   })
   
-  #Switch to graph tab once button in data tab is pressed.
+  # Switch to graph tab once button in data tab is pressed.
+  
   observeEvent(input$graphData, {
     if(is.null(input$csvFile)){
       #TODO: Dismiss this warning after some time.
@@ -46,37 +50,37 @@ shinyServer(function(input, output, session) {
     }
     updateTabItems(session, "menu", "graph")
   })
+  
+  # Calculate the regression once clicked and store the calculated values in a reactiveValues() so they can be accessed by other functions
       
-  #regressionValues = reactiveValues(xdata, ydata, slopevalues, interceptvalues, bestlineslope, bestlineintercept, slopeUncertainty, interceptUncertainty, highslope, highintercept, lowslope, lowintercept)
   regressionValues = reactiveValues()
   
   observeEvent(input$calculateFit, {
     
+    #TODO
     validate(
       need(input$setNumber >= 100 && input$setNumber <= 100000, "Please select a number from 100 to 100,000")
     )
     
     samples = input$setNumber
     
-    inputFile = input$csvFile
-    
-    if (is.null(inputFile))
+    if (is.null(input$csvFile))
       return(NULL)
     
     #Read uploaded data file. Uses header if option is selected in UI.
-    data = read.csv(inputFile$datapath, header = input$header)
+    data = read.csv(input$csvFile$datapath, header = input$header)
     
     normaldistribution = function(n, mean, uncertainty) {
       #Standard Deviation can be approximated to range/4 with 95% accuracy, range = 2*uncertainty, so sd equals approximately unc/2
       # Two or 3 standard deviations?
-      sd = uncertainty/3
+      # TODO
+      sd = uncertainty/2
       #rnorm() can take vectors (term for lists), will cycle through them
       result = rnorm(n*length(mean), mean = mean, sd = sd)
       #Create matrix of results. Each row is a point, and each column is a generated set of points.
       resultmatrix = matrix(data = result, ncol = n, byrow = FALSE)
       #Reorganize the result
       gatheredData = gather(as_data_frame(resultmatrix))
-      #melteddata[,1] = NULL
       return(gatheredData)
     }
     
@@ -110,8 +114,8 @@ shinyServer(function(input, output, session) {
     regressionValues$bestlineintercept = mean(regressionValues$interceptvalues)
     
     #TODO: Figure out number of standard deviations.
-    regressionValues$slopeUncertainty = 3*sd(regressionValues$slopevalues)
-    regressionValues$interceptUncertainty = 3*sd(regressionValues$interceptvalues)
+    regressionValues$slopeUncertainty = 2*sd(regressionValues$slopevalues)
+    regressionValues$interceptUncertainty = 2*sd(regressionValues$interceptvalues)
     
     regressionValues$highslope = regressionValues$bestlineslope + regressionValues$slopeUncertainty
     regressionValues$highintercept = regressionValues$bestlineintercept + regressionValues$interceptUncertainty
@@ -119,10 +123,9 @@ shinyServer(function(input, output, session) {
     regressionValues$lowslope = regressionValues$bestlineslope - regressionValues$slopeUncertainty
     regressionValues$lowintercept = regressionValues$bestlineintercept - regressionValues$interceptUncertainty
     
-    #plotclick = reactive(input$plot_click)
-    
     roundedSlopeError = signif(regressionValues$slopeUncertainty, 1)
     slopeExp = floor(log10(roundedSlopeError))
+    
     if(slopeExp > 0) {
       roundedSlope = round(regressionValues$bestlineslope, -slopeExp)
     } else {
@@ -131,17 +134,28 @@ shinyServer(function(input, output, session) {
     
     roundedInterceptError = signif(regressionValues$interceptUncertainty, 1)
     interceptExp = floor(log10(roundedInterceptError))
+    
     if(interceptExp > 0) {
       roundedIntercept = round(regressionValues$bestlineintercept, -interceptExp)
     } else {
       roundedIntercept = format(round(regressionValues$bestlineintercept, -interceptExp), nsmall = -interceptExp)
     }
     
-    # regressionValues$equationLabel = paste("Slope:\n", roundedSlope, "\u00B1", roundedSlopeError, 
-    #                                        "\nIntercept:\n", roundedIntercept, "\u00B1", roundedInterceptError)
+    # regressionValues$equationLabel = TeX(paste0("Slope: (", roundedSlope, " $\\pm$ ", roundedSlopeError, ")", input$yUnits, "/", input$xUnits,
+    #                                            "Intercept: (", roundedIntercept, " $\\pm$ ", roundedInterceptError, ")", input$yUnits), output = "character")
     
-    regressionValues$equationLabel = paste("Regression Equation:\n", "(", roundedSlope, "\u00B1", roundedSlopeError, ")",
-                                           "+", "(", roundedIntercept, "\u00B1", roundedInterceptError, ")")
+    # test12 = expression(alpha %+-% 5)
+    # 
+    # regressionValues$equationLabel = bquote(atop("Slope: " ( .(roundedSlope) %+-% .(roundedSlopeError) ) ~ .(test12), 
+    #                                              "Intercept: " ( .(roundedIntercept) %+-% .(roundedInterceptError) ) ))
+    
+    # test243 = TeX(input$yUnits)
+    # regressionValues$equationLabel = bquote("test" ~ .(test243[[1]]))
+    
+    # regressionValues$equationLabel = as.character(bquote(expression("hope this works" ~ .(test243[[1]]))))[[2]]
+    
+    regressionValues$slopeLabel = paste0("Slope: (", roundedSlope, "\u00B1", roundedSlopeError, ")")
+    regressionValues$interceptLabel = paste0("Intercept: (", roundedIntercept, "\u00B1", roundedInterceptError, ")")
   })
   
   
@@ -150,27 +164,51 @@ shinyServer(function(input, output, session) {
   
   observeEvent(input$plot_click, {
     clickValues$click = input$plot_click
+    # print("1. ~~~~~~~~~~~~~~~~~~~~")
+    # print(TeX(input$yUnits, output = "expression"))
+    # # print(TeX(input$yUnits, output = "ast"))
+    # print("2. ~~~~~~~~~~~~~~~~~~~~~~~")
+    # print(TeX(input$yUnits, output = "character"))
+    # print(toString(TeX(input$yUnits)))
+    # print(toString(TeX(input$yUnits, output = "character")))
+    # print("label:")
+    # print(TeX(input$yUnits, output = "text"))
+    # print(1)
+    # print(parse(text = deparse(TeX(input$yUnits, output = "character"))))
+    # print(2)
+    # print(TeX(input$yUnits, output = "character"))
+    # print(3)
+    # print(parse(text = TeX(input$yUnits, output = "character")))
+    # print(4)
+    #print(eval(TeX(input$yUnits)))
+    # print(TeX(input$xUnits, output = "character"))
+    # print(parse(text = TeX(input$xUnits, output = "character")))
+    # print(eval(parse(text = TeX(input$xUnits, output = "character"))))
   })
   
-  output$scatterPlot = renderPlot(height = 600, res = input$setPPI, {
+  #Ugly hack to make sure PPI stays the same across screen sizes
+  #1218 is standard width on 1920 x 1080 screen
+  ppi = function() {
+    (session$clientData$output_scatterPlot_width / 1218) * (150 * (input$setPPI / 100))
+  }
+  
+  plot1 = reactive({
     
-    inputFile = input$csvFile
-    
-    if (is.null(inputFile)){
+    if (is.null(input$csvFile)){
       return(NULL)
     }
     
-    data = read.csv(inputFile$datapath, header = input$header)
+    data = read.csv(input$csvFile$datapath, header = input$header)
     
     yerrors = aes(ymax = data[,3] + data[,4], ymin = data[,3] - data[,4])
     xerrors = aes(xmax = data[,1] + data[,2], xmin = data[,1] - data[,2])
     
-    #TODO: Fix slight error in graph limits
+    #TODO: Fix this hack
     #A plot used to calculte limits that actually isn't shown
     #The error bars have 0 width/height because they won't be seen but need to be used to calculate limits
-    plotframe = 
+    plotframe =
       qplot(data[,1], data[,3]) +
-      geom_errorbar(yerrors, width = 0)  + 
+      geom_errorbar(yerrors, width = 0)  +
       geom_errorbarh(xerrors, height = 0)
     
     maxydata = layer_scales(plotframe)$y$range$range[2]
@@ -181,7 +219,7 @@ shinyServer(function(input, output, session) {
     xrange = maxxdata - minxdata
     yrange = maxydata - minydata
     
-    #TODO possibly change constant depending upon aspect ratio so one isn't larger, do this for error bars too
+    #TODO change constant depending upon aspect ratio, do this for error bars too
     ybuffer = 0.05*yrange
     xbuffer = 0.05*xrange
     
@@ -200,18 +238,26 @@ shinyServer(function(input, output, session) {
     #TODO: Figure out issues with selecting two different CSV in a row
     
     if(!is.na(input$xMin) & !is.na(input$yMin) & !is.na(input$xMax) & !is.na(input$yMax)) {
-      regressionPlot = 
-        qplot(data[,1], data[,3]) + 
-        geom_errorbar(yerrors, width = 0.02*(xrange))  + 
-        geom_errorbarh(xerrors, height = 0.02*(yrange)) + 
+      regressionPlot =
+        qplot(data[,1], data[,3]) +
+        geom_errorbar(yerrors, width = 0.02*(xrange))  +
+        geom_errorbarh(xerrors, height = 0.02*(yrange)*(1/as.numeric(input$aspectRatio))) +
         eval(parse(text = input$selectTheme)) +
         theme(aspect.ratio = input$aspectRatio, plot.background=element_blank()) +
         ggtitle(TeX(input$graphTitle)) +
-        xlab(TeX(input$xLabel)) +
-        ylab(TeX(input$yLabel)) +
+        xlab(if(input$xUnits != "") {
+          TeX(paste0(input$xLabel, " (", input$xUnits, ")"))
+        } else {
+          TeX(input$xLabel)
+        }) +
+        ylab(if(input$yUnits != "") {
+          TeX(paste0(input$yLabel, " (", input$yUnits, ")"))
+        } else {
+          TeX(input$yLabel)
+        }) +
         xlim(input$xMin, input$xMax) +
-        ylim(input$yMin, input$yMax)
-      
+        ylim(input$yMin, input$yMax) +
+        scale_size_identity()
     } else {
       return(NULL)
     }
@@ -226,15 +272,18 @@ shinyServer(function(input, output, session) {
     }
     
     if(input$showGenerated == TRUE & !is.null(regressionValues$xdata)){
-      regressionPlot$layers = c(geom_point(aes(regressionValues$xdata[,2], regressionValues$ydata[,2]), color = input$dataColor, alpha = 1/20), regressionPlot$layers)
+      regressionPlot$layers = c(geom_point(aes(regressionValues$xdata[,2], regressionValues$ydata[,2]), 
+                                           color = input$dataColor, alpha = 1/20), regressionPlot$layers)
     }
     
     if(input$showSpread == TRUE & !is.null(regressionValues$interceptvalues)){
-      regressionPlot$layers = c(geom_abline(intercept = regressionValues$interceptvalues, slope = regressionValues$slopevalues, alpha = 1/10, color = input$spreadColor), regressionPlot$layers)
+      regressionPlot$layers = c(geom_abline(intercept = regressionValues$interceptvalues, slope = regressionValues$slopevalues, 
+                                            alpha = 1/10, color = input$spreadColor), regressionPlot$layers)
     }
     
     if(!is.null(regressionValues$bestlineslope)){
-      regressionPlot$layers = c(regressionPlot$layers, geom_abline(intercept = regressionValues$bestlineintercept, slope = regressionValues$bestlineslope))
+      regressionPlot$layers = c(regressionPlot$layers, geom_abline(intercept = regressionValues$bestlineintercept, 
+                                                                   slope = regressionValues$bestlineslope))
     }
     
     if(input$showMaxMin == TRUE & !is.null(regressionValues$highintercept)){
@@ -242,27 +291,60 @@ shinyServer(function(input, output, session) {
                                                                    linetype = 3), geom_abline(intercept = regressionValues$lowintercept, slope = regressionValues$highslope, linetype = 3))
     }
     
-    if(input$showEquationFloat == TRUE){
-      regressionPlot$layers = c(regressionPlot$layers, annotate("label", x = clickValues$click$x, y = clickValues$click$y,  label.r = unit(0, "lines"), label = regressionValues$equationLabel))
+    # TODO: Fix label size
+    if(input$showEquationFloat == TRUE & !is.null(regressionValues$slopeLabel)){
+      if(input$yUnits != "" || input$xUnits != "") {
+        equationLabel = as.character(bquote(expression(atop(.(regressionValues$slopeLabel) ~ .(TeX(input$yUnits)[[1]]) * "/" * .(TeX(input$xUnits)[[1]]), 
+                                                            .(regressionValues$interceptLabel) ~ .(TeX(input$yUnits)[[1]])))))[[2]]
+        parseMath = TRUE
+      } else {
+        equationLabel = paste0(regressionValues$slopeLabel, "\n", regressionValues$interceptLabel)
+        parseMath = FALSE
+      }
+     
+      # regressionPlot$layers = c(regressionPlot$layers, annotate("label", x = clickValues$click$x, y = clickValues$click$y,  
+      #                                                           label.r = unit(0, "lines"), label = equationLabel, parse = parseMath, hjust = "left", size = 5 * (input$setPPI / 100)))
+      
+      regressionPlot$layers = c(regressionPlot$layers, geom_label(aes(x = clickValues$click$x, y = clickValues$click$y), 
+                                                                label.r = unit(0, "lines"), label.padding = unit(0.30, "lines"), label = equationLabel, parse = parseMath, hjust = "left", 
+                                                                size = (input$setLabelScale / 100) * 5 * (1/(sqrt(ppi() / 100)))))
     }
     
-    observe({
-      if(!is.null(inputFile)) {
-        shinyjs::enable("downloadPlot")
-      }
-    })
-    
-    output$downloadPlot = downloadHandler(
-      filename = function() {paste0(str_replace(input$csvFile, ".csv", ""), '.', input$fileFormat)},
-      content = function(file) {
-        ggsave(file, plot = regressionPlot, device = input$fileFormat)
-      }
-    )
-    
-    return(regressionPlot)
+    regressionPlot
     
   })
   
+  
+  
+  observeEvent({
+    input$setPPI
+    session$clientData$output_scatterPlot_width}, {
+    output$scatterPlot = renderPlot(height = 600, res = ppi(), {
+      
+      plot1()
+      
+    })
+  })
+  
+  output$downloadPlot = downloadHandler(
+    filename = function() {paste0(str_replace(input$csvFile, ".csv", ""), '.', input$fileFormat)},
+    content = function(file) {
+      width = 1218 / input$setPPI
+      switch(input$fileFormat, 
+             "pdf" = {
+               pdf(file, width = width, height = width * as.numeric(input$aspectRatio))
+             },
+             "png" = {
+               png(file, width = as.numeric(input$downloadResolution), height = as.numeric(input$downloadResolution) * as.numeric(input$aspectRatio), 
+                   res = (as.numeric(input$downloadResolution) / 1218) * as.numeric(input$setPPI))
+             }, 
+             "svg" = {
+               svg(file, width = width, height = width * as.numeric(input$aspectRatio))
+             })
+      print(plot1())
+      dev.off()
+    }
+  )
   
 })
   
--- Web_Linreg/ui.R
+++ Web_Linreg/ui.R
@@ -21,7 +21,7 @@ body = dashboardBody(
       fluidRow(
         box(width = "9", DT::dataTableOutput("dataTable")),
         box(width = "3",
-            helpText( a(icon("question"), "File formatting", href = "https://github.com/joshkraan/linear-regression-uncertainties/wiki/Web-Version-Usage", target = "_blank")),
+            helpText( a(icon("question"), "File formatting", href = "https://github.com/joshkraan/linear-regression-uncertainties/wiki/Web-Version-Usage#file-formatting", target = "_blank")),
             fileInput("csvFile", "Upload a CSV data file", accept = c('text/csv', 'text/comma-separated-values', '.csv')),
             checkboxInput('header', 'Header Row', FALSE),
             div(style = "text-align: center", actionButton("graphData", "Graph Data", width = '100%')),
@@ -31,7 +31,7 @@ body = dashboardBody(
     }),
     tabItem(tabName = "graph", {
       fluidRow(
-        box(width = "9", plotOutput("scatterPlot", height = "auto", click = "plot_click")),
+        box(width = "9", plotOutput("scatterPlot", width = "auto", height = "auto", click = "plot_click")),
         tabBox(width = "3",
           tabPanel(icon("cogs"),
                    textInput("graphTitle", "Title", value = NULL, width = "100%"),
@@ -46,17 +46,19 @@ body = dashboardBody(
                                               "This means that many regular LaTeX math formulas can be used. Rather than one ", 
                                               "backslash two should be used for commands. See the latex2exp documentation for more information."),
                              placement = "left"),
+                   textInput("xUnits", "X Axis Units", value = NULL, width = "100%"),
                    textInput("yLabel", "Y Axis Label", value = "Y", width = "100%"),
                    bsPopover(id = "yLabel", title = "Using LaTeX", 
                              content = paste0("The graph axis labels are processed using the R package latex2exp. ", 
                                               "This means that many regular LaTeX math formulas can be used. Rather than one ", 
                                               "backslash two should be used for commands. See the latex2exp documentation for more information."),
                              placement = "left"),
+                   textInput("yUnits", "Y Axis Units", value = NULL, width = "100%"),
+                   hr(),
                    splitLayout(
                      numericInput('xMin', 'X Min', ''), #TODO: Fix issues with steps and invalid value error message on mouseover
                      numericInput('yMin', 'Y Min', '')
                    ),
-                   
                    splitLayout(
                      numericInput('xMax', 'X Max', ''),
                      numericInput('yMax', 'Y Max', '')
@@ -70,10 +72,10 @@ body = dashboardBody(
                    actionButton("calculateFit", "Calculate Fit", width = '100%')),
           tabPanel(icon("paint-brush"),
                    selectInput("aspectRatio", "Aspect Ratio", 
-                               c("16:9" = 9/16, "4:3" = 3/4, "1:1" = 1)),
-                   numericInput("setPPI", "PPI", value = 100, min = 50, max = 250),
+                               c("16:9" = 9/16, "4:3" = 3/4, "1:1" = 1), width = "100%"),
+                   sliderInput("setPPI", "Element Scale", min = 50, max = 200, value = 100, post = "%", width = "100%"),
                    selectInput("selectTheme", "Theme", 
-                               c("Black & White" = "theme_bw()", "Grey" = "theme_grey()", "Base" = "theme_base()", "Google Docs" = "theme_gdocs()", "LibreOffice" = "theme_calc()")),
+                               c("Black & White" = "theme_bw()", "Grey" = "theme_grey()", "Base" = "theme_base()", "Google Docs" = "theme_gdocs()", "LibreOffice" = "theme_calc()"), width = "100%"),
                    tags$hr(),
                    checkboxInput("showMaxMin", "Show Max/Min Lines", value = FALSE),
                    checkboxInput("showSpread", "Show Spread", value = FALSE),
@@ -89,13 +91,17 @@ body = dashboardBody(
                    checkboxInput("showEquationFloat", "Show Equation On Graph", value = FALSE),
                    conditionalPanel(
                      condition = "input.showEquationFloat == true",
-                     helpText("Position the equation by clicking on the graph.")
+                     helpText("Position the equation by clicking on the graph."),
+                     sliderInput("setLabelScale", "Label Scale", min = 50, max = 150, value = 100, post = "%", width = "100%")
                    )),
           tabPanel(icon("download"),
-                   selectInput("downloadResolution", "Download Resolution", 
-                               c("1080p", "720p", "480p")),
                    selectInput("fileFormat", "Download File Format", 
                                c("PDF" = "pdf", "SVG" = "svg", "PNG" = "png")),
+                   conditionalPanel(
+                     condition = "input.fileFormat == 'png'",
+                     selectInput("downloadResolution", "Download Resolution", 
+                                 c("1080p" = 1920, "720p" = 1280, "480p" = 640))
+                   ),
                    downloadButton("downloadPlot", "Download"),
                    tags$style(type='text/css', '#downloadPlot { width:100%; align: center'))
         )
